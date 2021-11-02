680bd7191d93ca6c0dd073fa18f10e835715ed48
TMats <takomats@hotmail.com>
2021-11-02 15:05:02 -0400

    got the volume bar and volume number showing up.

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ initializePlayer = () => {
     // Set object references
     vid = document.getElementById('player');
     playBtn = document.getElementById('playPauseBtn');
-    autoBtn = document.getElementById('autoPlay')
-    disableAutoBtn = document.getElementById('disableAutoPlay')
+    autoBtn = document.getElementById('autoPlay');
+    disableAutoBtn = document.getElementById('disableAutoPlay');
+    volumeControl = document.getElementById('vol-control');
+    volume = document.getElementById('volume');
+
     // Add event listeners
     playBtn.addEventListener("click", playPause, false);
     autoBtn.addEventListener("click", autoPlay, false);
     disableAutoBtn.addEventListener("click", disableAutoPlay, false);
+    volumeControl.addEventListener("click", setVolume, false);
 }
 // Makes sure html runs first before javascript
 window.onload = initializePlayer;
@@ -34,4 +38,10 @@ autoPlay = () => {
 disableAutoPlay = () => {
     vid.autoplay = false;
     vid.load();
+}
+
+// Set Volume
+setVolume = (val) => {
+    vid.volume = val / 100;
+    volume.innerHTML = ('Volume: ' + Math.floor(vid.volume * 100))
 }
